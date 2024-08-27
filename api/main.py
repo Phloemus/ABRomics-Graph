199593abf7a40644355a50ddd106a973b3d9e907
Brieuc Quemeneur <brieuc.quemeneur@univ-nantes.fr>
2024-08-27 17:39:36 +0200

    build: correcting the some fields for the sample for the graph data to better fit the choosen ontologies
    Former-commit-id: b786c75607d95dda6b0595290458c1bd4cbbb7c3

--- api/main.py
+++ api/main.py
@@ -1,9 +1,14 @@
-from typing import Union
+#!/usr/bin/env python3
 
+
+## Necessary imports
+import os
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
-
 
 @app.get("/")
 def root():
@@ -13,3 +18,23 @@ def root():
 @app.get("/items/{item_id}")
 def item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+@app.get("/count-nodes")
+def countNodes():
+    sparql_query = """
+       SELECT ?graph (COUNT(?s) AS ?triples)
+       WHERE {
+          GRAPH ?graph {
+            ?s ?p ?o .
+          }
+        }
+        GROUP BY ?graph
+    """
+    sparql = SPARQLWrapper("http://localhost:8081/sparql")
+    sparql.setReturnFormat(JSON)
+    sparql.setQuery(sparql_query)
+    try:
+        res = sparql.query().convert()
+        return res
+    except Exception as e:
+        return {"error-message": e}
