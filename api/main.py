13449b026982c05e8bd0a5bd37f2e787d88c2e35
Brieuc Quemeneur <brieuc.quemeneur@univ-nantes.fr>
2024-09-10 11:36:02 +0200

    build:adding the api that allows to perform requests on the kg
    Former-commit-id: 4f276a224630fc82d982c2df293c57e35158f290

--- api/main.py
+++ api/main.py
@@ -8,7 +8,7 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 from typing import Union
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(debug=True)
 
 @app.get("/")
 def root():
@@ -19,7 +19,7 @@ def root():
 def item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
-@app.get("/count-nodes")
+@app.get("/count-nodes") 
 def countNodes():
     sparql_query = """
        SELECT ?graph (COUNT(?s) AS ?triples)
@@ -35,6 +35,43 @@ def countNodes():
     sparql.setQuery(sparql_query)
     try:
         res = sparql.query().convert()
-        return res
+        recs = res["results"]["bindings"]
     except Exception as e:
         return {"error-message": e}
+    response = list()
+    for item in recs:
+        response.append({"graphName": item["graph"]["value"], "nbTriples": item["triples"]["value"]})
+    return response
+
+
+## The specie name could be the common name or latin name
+@app.get("/get-organs/{specie_name}") 
+def getOrgans(specie_name: str):
+    sparql_query = f"""
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX ncbitaxon: <http://purl.obolibrary.org/obo/NCBITaxon_>
+        PREFIX uberon: <http://purl.obolibrary.org/obo/UBERON_>
+        PREFIX obom: <https://data.bioontology.org/metadata/obo/>
+        PREFIX ro: <http://purl.obolibrary.org/obo/RO_>
+        
+        SELECT ?specie ?organs ?organLabels WHERE {{
+          ?specie rdfs:label "{ specie_name }" .
+          ?organs ro:0002175 ?specie . ## ro:0002175 indicates the "present_in_taxon" ontology
+          ?organs rdfs:label ?organLabels .
+        }}
+    """
+    sparql = SPARQLWrapper("http://localhost:8081/sparql")
+    sparql.setReturnFormat(JSON)
+    sparql.setQuery(sparql_query)
+    try:
+        res = sparql.query().convert()
+        recs = res["results"]["bindings"]
+    except Exception as e:
+        return {"error-message": e}
+    response = list()
+    for item in recs:
+        response.append({"specie": item["specie"]["value"], "organ": item["organs"]["value"], "organName": item["organLabels"]["value"]})
+    return response
+
+
