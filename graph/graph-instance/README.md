1cdd3d5345180fab693bc5173a24eadcd1020c3b
Brieuc Quemeneur <brieuc.quemeneur@univ-nantes.fr>
2024-09-11 14:59:38 +0200

    build: correcting a bug that made the virtuoso unable to load the data from the .ttl files the virtuoso docker container + curated the conda environment of the streamlit demo app

---
+++ graph/graph-instance/README.md
@@ -0,0 +1,4 @@
+## DO not delete this folder or virtuoso won't work
+
+Virtuoso uses this folder to store the .ttl files used in the graph (it stores it temporarly. But the graph-instance is still mendatory for
+virtuoso to work)
--- graph/set_up.sh
+++ graph/set_up.sh
@@ -1,5 +1,5 @@
 # Tracks the data of these files
-echo "ld_dir_all('/usr/local/virtuoso-opensource/share/virtuoso/vad/data', '*.ttl', 'http://data.abromics.fr');" | isql-v -U dba -P dba 
+echo "ld_dir_all('/usr/local/virtuoso-opensource/share/virtuoso/vad/graph-instance', '*.ttl', 'http://data.abromics.fr');" | isql-v -U dba -P dba 
 echo "ld_dir_all('/usr/local/virtuoso-opensource/share/virtuoso/vad/ontologies', '*.owl', 'http://ontologies.abromics.fr');" | isql-v -U dba -P dba 
 
 # Allows to load the graph data (fetch data from the graph files tracked before)
