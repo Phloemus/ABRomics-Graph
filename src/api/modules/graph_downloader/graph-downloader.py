3c6d791e4b1ace50bac5ee4fbab6239b50c1d50a
Brieuc Quemeneur <brieuc.quemeneur@univ-nantes.fr>
2024-11-07 17:20:40 +0100

    build: adding the script that filters only the right reports (Claudine, public)
    Former-commit-id: 19aa40986d5072c405109afa2ec6356044a5f1f9

--- src/api/modules/graph_downloader/graph-downloader.py
+++ src/api/modules/graph_downloader/graph-downloader.py
@@ -2,6 +2,9 @@
 from alive_progress import alive_bar
 import time
 import requests
+import os
+import json
+from dotenv import load_dotenv
 
 
 ## Downloader class allows to fetch abromics reports from the api, filter them 
@@ -14,10 +17,27 @@ class Downloader():
         load_dotenv()
         
         ## load the environment variables
-        self.api_url = os.getenv('API_URL') 
-        self.api_user_token = os.getenv('API_USER_TOKEN')
+        self.api_url = "https://analysis.abromics.fr" # os.getenv('API_URL') 
+        self.email = ""
+        self.password = ""
+        self.api_user_token = "YWRtaW5AYW5hbHlzaXMuYWJyb21pY3MuZnI6YWJyb21pY3MyMDIz" # This is the master basic token # os.getenv('API_USER_TOKEN')
 
     ## Public methods
+
+    def getUserToken(self):
+        response = requests.post(
+            "https://analysis.abromics.fr/api/login", 
+            data={
+                "email": self.email, 
+                "password": self.password
+            }
+        )
+        try:
+            response = response.json()
+        except:
+            print(f"Invalid Credentials: {response}")
+            exit()
+        self.api_user_token = response.access
 
     ## download all the abromics reports marked as "ready to report" 
     ## downloadDir (string) : indicate the directory in which the reports should be saved
@@ -29,10 +49,11 @@ class Downloader():
                 response["next"],
                 params={'status': 'ready_to_report'},
                 headers = {
-                    'Authorization': f"Basic {self.api_user_token}",
+                    'Authorization': f"Basic {self.api_user_token}", ## replace Basic with Bearer if it's a Bearer token
                 }
             )
             response = response.json()
+            ## print(response)
             for analysis in response["results"]:
                 exploitable_analysis_ids.append(analysis["id"])
 
@@ -42,21 +63,27 @@ class Downloader():
             os.makedirs(downloadDir)
     
         with alive_bar(len(exploitable_analysis_ids)) as bar:
+            countDownloadFails = 0
+            countDownloadSuccess = 0
             for report_id in exploitable_analysis_ids:
                 print(report_id)
                 response = requests.get(
                     f"https://analysis.abromics.fr/api/analysis/{report_id}/report/",
                     headers = {
-                        'Authorization': f"Basic {self.api_user_token}",
+                        'Authorization': f"Basic {self.api_user_token}", ## replace Basic with Bearer if it's a Bearer token
                     }
                 )
-                report = response.json()
-                
-                # save the reports in a local file
-                with open(f'{downloadDir}/abr_report_{report_id}.json', 'w') as f:
-                    json.dump(report, f)
+                try:
+                    report = response.json()
+                    # save the reports in a local file
+                    with open(f'{downloadDir}/abr_report_{report_id}.json', 'w') as f:
+                        json.dump(report, f)
+                    countDownloadSuccess += 1
+                except:
+                    print("report not downloaded - Wrong format")
+                    countDownloadFails += 1
                 bar()
-
+            print(f"Download finish ! \nnb success: {countDownloadSuccess}\nnb fails: {countDownloadFails}")
 
 
 ## Download all the abromics reports marked as ready to report
