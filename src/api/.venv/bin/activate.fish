f0969f8c4083edfd27cb4becd8ffddae89092070
Phloemus <brieucquemeneur@gmail.com>
2025-03-10 17:38:16 +0100

    build: making the requirements.txt file to the right format, but some packages are still missing

--- src/api/.venv/bin/activate.fish
+++ src/api/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/brieuc/Documents/dev/abromics/abromics-kg/src/api/.venv"
+set -gx VIRTUAL_ENV "/home/brieuc/Documents/dev/abromics/abromics-graph/src/api/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
