0043765e6660135c22fe3dd13a68b4b4ebcd3f95
Phloemus <brieucquemeneur@gmail.com>
2025-03-17 16:57:11 +0100

    build: making the api accept cors requests from anywhere
    Former-commit-id: 16052db07aea82e6c88886d9161bf3f5d1635fbf

--- src/web/src/app.vue
+++ src/web/src/app.vue
@@ -1,5 +1,33 @@
+<script setup>
+    import { ref } from 'vue'
+    const count = ref(0)
+
+    function incrementBy2() {
+        count.value += 2 
+    }
+
+    function fetchQueryResult(id) {
+        console.log(id)
+        fetch("http://localhost:5000/graph-api/node/count").then(response => {
+            return response.json()
+        }).then((data) => {
+            console.log(data)
+        }).catch(err => {
+            console.error("Error fetching data: ", err)
+        })
+    }
+</script>
+
+
 <template>
   <div>
-      <h1>Hello world</h1>
+      <h1>ABRomics KG</h1>
+      <button
+          class="px-6 py-1 bg-slate-900 text-white text-md rounded-sm"
+          @click="incrementBy2"
+      >
+        count is {{ count }}
+      </button>
+      <button @click="fetchQueryResult(1)">Execute Query 1</button>
   </div>
 </template>
