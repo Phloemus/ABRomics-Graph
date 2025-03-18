c3f8d042d7af377f82982597d633422c2b50dbc0
Phloemus <brieucquemeneur@gmail.com>
2025-03-18 17:13:19 +0100

    build: created a nice looking layout UI for the webapp of abromics kg

--- src/web/src/app.vue
+++ src/web/src/app.vue
@@ -1,10 +1,5 @@
 <script setup>
     import { ref } from 'vue'
-    const count = ref(0)
-
-    function incrementBy2() {
-        count.value += 2 
-    }
 
     function fetchQueryResult(id) {
         console.log(id)
@@ -21,14 +16,51 @@
 
 
 <template>
-  <div>
-      <h1>ABRomics KG</h1>
-      <button
-          class="px-6 py-1 bg-slate-900 text-white text-md rounded-sm"
-          @click="incrementBy2"
-      >
-        count is {{ count }}
-      </button>
-      <button @click="fetchQueryResult(1)">Execute Query 1</button>
-  </div>
+    <div class="w-full h-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500"></div>
+    <div class="h-screen w-full bg-slate-50 flex">
+        <div class="px-4 w-96 h-full bg-white border-r border-slate-200 z-20">
+            <div>
+
+            </div>
+            <div class="w-full">
+                <h3 class="mx-1 my-4 text-slate-600 text-lg">Queries</h3>
+                <div class="my-3 p-2 bg-slate-50 rounded-md">
+                    <h4 class="text-slate-800 text-md">
+                        <span class="font-medium text-slate-900">Competency Question 1:</span> 
+                        What are the most common antibiotic resistance genes present in all samples
+                    </h4>
+                </div>
+                <div class="my-3 p-2 bg-slate-50 rounded-md">
+                    <h4 class="text-slate-800 text-md">
+                        <span class="font-medium text-slate-900">Competency Question 1:</span> 
+                        What are the most common antibiotic resistance genes present in all samples
+                    </h4>
+                </div>
+                <div class="flex justify-center">
+                    <button
+                        class="px-8 py-2 bg-slate-900 hover:bg-slate-800 text-white text-md rounded-md"
+                    >
+                        More queries
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="w-full">
+            <nav class="px-6 py-3 w-full bg-white border-b border-slate-200">
+
+                <div class="flex flex-row-reverse gap-4">
+                    <button
+                       class="px-8 py-2 bg-slate-900 hover:bg-slate-800 text-white text-md rounded-md"
+                    >
+                        Login
+                    </button>
+                </div>
+            </nav>
+            <main class="p-6">
+                <h1>ABRomics KG</h1>
+                <button @click="fetchQueryResult(1)">Execute Query 1</button>
+
+            </main>
+        </div>
+    </div>
 </template>
