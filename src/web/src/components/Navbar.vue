3e6bc7023ac12d1d35db225dcff888fb080015a5
Phloemus <brieucquemeneur@gmail.com>
2025-03-20 17:27:37 +0100

    build: making components from the main page already written

---
+++ src/web/src/components/Navbar.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav class="px-6 py-3 w-full bg-white border-b border-slate-200">
+        <div class="flex flex-row-reverse gap-4">
+            <ActionButton content="Login"/>
+        </div>
+    </nav>
+</template>
