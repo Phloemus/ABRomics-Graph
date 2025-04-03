717e80352e5cb9583753bb70140cee6c6c4d1b6a
Phloemus <brieucquemeneur@gmail.com>
2025-04-03 16:50:03 +0200

    build: creating a nuxt component out of the small search component
    Former-commit-id: 59a20027dfae96e951a77630380d7d007d747e37

--- src/web/src/pages/index.vue
+++ src/web/src/pages/index.vue
@@ -1,9 +1,43 @@
 <script>
 
+    import SearchInput from "../components/SearchInput.vue"
+
+    // import data from "../static/result-10k-first-classes.json"
+    import data from "../static/test-classes.json"
+
+    const searchTerm = ref("")
+    const organs = ref([])
+
+    data.results.bindings.forEach((data) => {
+        organs.value.push({ "name": data.organLabels.value, "ontology": data.organs.value })
+    })
+
+    function filterOntologyList(event) {
+        console.log("hahaha")
+        searchTerm.value = event.target.value
+        console.log(searchTerm.value)
+        const query= searchTerm.value.toLowerCase();
+        organs.value = []
+        data.results.bindings.forEach(suggestion => {
+            if(suggestion.organLabels.value.toLowerCase().includes(query)) {
+                const elem = {
+                    "name": suggestion.organLabels.value,
+                    "ontology": suggestion.organs.value
+                }
+                organs.value.push(elem)
+            }
+        });
+    }
 
 </script>
 
-
 <template>
-    <h1>Hiii</h1>
+
+    <SearchInput 
+        :searchTerm="searchTerm"
+        @input="filterOntologyList"
+        placeholder="Searching an ontology term.."
+        :results="organs"
+    />
+
 </template>
