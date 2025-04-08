da0270c0fea0c25a8bd8756a34f86a2a071d999b
Phloemus <brieucquemeneur@gmail.com>
2025-04-08 17:17:03 +0200

    build: reformating the api file structure and making the query with parameter works for the get sample sources api route
    Former-commit-id: dc739e9c82a27c489fbd94a089392eebce85ecc8

---
+++ src/api/src/routes/abromics.py
@@ -0,0 +1,29 @@
+
+## ABRomics api routes
+##
+## The routes of the API that are used by the abromics platform
+##
+## routes:
+## - [GET] 
+## 
+
+
+from flask_cors import CORS, cross_origin
+from flask import jsonify, request
+
+## Config imports
+from config.config import *
+from config.constants import *
+
+
+## Module imports
+from utils.query import Query
+
+
+@cross_origin()
+@app.route(f"/{API_BASEPATH}/sample-sources", methods=[QUERIES[5]["method"]])
+def getSampleSources():
+    specieName = request.json["specieName"]
+    query = Query(QUERIES[5]["filePath"], sparqlEndpoint=SPARQL_ENDPOINT, parameters={"specieName": "Homo sapiens"})
+    return jsonify(query.executeQuery())
+
