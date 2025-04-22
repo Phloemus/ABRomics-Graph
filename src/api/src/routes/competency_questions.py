772aa925a3a73e7ad5f3d5f9eff1bc0b20ab0e51
Phloemus <brieucquemeneur@gmail.com>
2025-04-22 17:00:30 +0200

    build: updating the stat routes of the api, adding a compressed archive of the ontologies used
    Former-commit-id: 4bee9d34f6b699524cbd15d12ae2f795fd0c97b9

--- src/api/src/routes/competency_questions.py
+++ src/api/src/routes/competency_questions.py
@@ -18,19 +18,4 @@ from config.constants import *
 ## Util imports
 from utils.query import Query
 
-
-@cross_origin()
-@app.route(f"/{API_BASEPATH}/organ", methods=[QUERIES[3]["method"]])
-def listAvailableOrgansForSpecieName():
-    specieName = request.json["specie_name"]
-    query_parameters = [ {"string_to_replace": "Homo sapiens", "values": [specieName]} ]
-    return jsonify(executeQuery(SPARQL_ENDPOINT, QUERIES[3]["filePath"], query_parameters))
-
-## Compentency questions
-@cross_origin()
-@app.route(f"/{API_BASEPATH}/get-ktop-antibiotic-res-genes", methods=[QUERIES[4]["method"]])
-def getKTopAntibioticResGenes():
-    metric = request.json["metric"]
-    query_parameters = [ {"string_to_replace": "Gene Length", "values": [metric]} ]
-    return jsonify(executeQuery(SPARQL_ENDPOINT, QUERIES[4]["filePath"]))
-
+## Competency questions
