3e5781c6853110cbc56c55ab845e5937d3f9be03
Phloemus <brieucquemeneur@gmail.com>
2025-10-14 11:19:00 +0200

    build: fixed the query result table in the web interface

--- src/web/src/components/Table.vue
+++ src/web/src/components/Table.vue
@@ -1,8 +1,12 @@
 <script setup>
 
-    defineProps({
+    const props = defineProps({
         data: Array,
     })
+    console.log(props.data)
+
+    const columns = Object.keys(props.data[0])
+    console.log(columns)
 
 </script>
 
@@ -12,10 +16,10 @@
         <table class="my-4 w-full">
             <tbody class="w-full bg-slate-50 border border-slate-100 rounded-md">
                 <tr class="bg-white border border-slate-100 font-medium text-slate-900 text-lg rounded-t-md">
-                    <th v-for="(key, index) in data[0].triples.keys" class="p-2 border border-slate-100">{{ key }}</th>
+                    <th v-for="(key, index) in columns" class="p-2 border border-slate-100">{{ key }}</th>
                 </tr>
                 <tr v-for="(row, index) in data">
-                    <td v-for="(value, index) in row" class="p-2 border border-slate-100">{{ value }}</td>
+                    <td v-for="(value, index) in row" class="p-2 border border-slate-100">{{ value.value }}</td>
                 </tr>
             </tbody>
         </table>
