d8aa31a42f3dd8d7232af2208effad9b69a55bae
Phloemus <brieucquemeneur@gmail.com>
2025-11-10 13:24:15 +0100

    build: adding the list for the environmenal medium terms

--- utils/metadata-lists/environmental-medium/script.py
+++ utils/metadata-lists/environmental-medium/script.py
@@ -4,12 +4,15 @@ def readJson(filename):
     ontologies = {}
     with open(filename, 'r') as file:
         data = json.load(file)
+        count = 1
         for item in data:
+            item["pk"] = count
             if "FOODON" in item["fields"]["class"]:
                 item["fields"]["ontology"] = "FOODON"
+            count = count + 1
         json_str = json.dumps(data, indent=4)
     with open("out.json", 'w') as f:
         f.write(json_str)
 
-readJson("environmental-medium-terms-final.json")
+readJson("unformated-list.json")
 
