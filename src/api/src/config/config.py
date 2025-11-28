626e5718bb442c824c7e01befef0f7740b2b9018
Phloemus <brieucquemeneur@gmail.com>
2025-11-28 10:47:15 +0100

    build: changed the nginx config to accept redirect /sparql request to webapp or to graphdb. Also changed the virtuoso env variables to graph server ones

--- src/api/src/config/config.py
+++ src/api/src/config/config.py
@@ -16,7 +16,7 @@ if "IS_DEV" in os.environ and os.environ['IS_DEV'] == "false": ##? IS_DEV might 
     API_PORT = f"{os.environ['API_PORT']}"
     API_ENDPOINT = f"{os.environ['HTTP']}{os.environ['API_HOST']}:{os.environ['API_PORT']}"
     API_BASEPATH = f"{os.environ['API_BASEPATH']}"
-    SPARQL_ENDPOINT = f"{os.environ['VIRTUOSO_HOST']}/sparql"
+    SPARQL_ENDPOINT = f"{os.environ['GRAPH_SERVER_HOST']}/sparql"
     ADMIN_USERNAME = f"{os.environ['API_ADMIN_USERNAME']}"
     ADMIN_PASSWORD = f"{os.environ['API_ADMIN_PASSWORD']}"
 else:    
