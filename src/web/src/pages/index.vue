3cae356ccb7187c8037fa90777b9a816d6089eef
Phloemus <brieucquemeneur@gmail.com>
2025-12-01 17:02:43 +0100

    build: adding the documentation to the webpage

--- src/web/src/pages/index.vue
+++ src/web/src/pages/index.vue
@@ -25,6 +25,18 @@
                 <li>Antibiotic resistances by country</li>
                 <li>Antibiotic resistances in different timeframes</li>
             </ul>
+            <h2 class="mt-10 mb-6 text-xl text-slate-900 font-bold">Dataset</h2>
+            <p class="my-6 text-lg text-slate-700 leading-8">
+                The knowledge graph has been created using public metadata and antibiotic resistance analysis data from the ABRomics plateform.
+            </p>
+            <p class="my-6 text-lg text-slate-700 leading-8">
+                The genomic sequences and metadata of multiple harmfull strains of bacterias found in human, animal and environmental origins have been integrated and processed into the ABRomics platform. The resulting 1613 analysis reports gather sample metadata as well as antibiotic resistance genes detected with the ABRomics bioinformatics workflows were then extracted and formated using the graph structure described below.
+            </p>
+            <h2 class="mt-10 mb-6 text-xl text-slate-900 font-bold">Knowledge graph structure</h2>
+            <div class="px-10 w-full flex flex-col gap-10">
+                <img src="assets/sample-modelisation-sosa.png"/>
+                <img src="assets/measure-modelisation-sosa.png"/>
+            </div>
         </div>
     </div>
 </template>
