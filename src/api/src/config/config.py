13e3576c59730e732449fde63ebcff49d0694e83
prod server <brieuc.quemeneur@univ-nantes.fr>
2025-12-12 14:51:11 +0100

    build: modified the env variables for the graph server host for dev and prod and modified slightly the SPARQL_ENDPOINT config variable in the api to fit the env GRAPH_SERVER_HOST changes

--- src/api/src/config/config.py
+++ src/api/src/config/config.py
@@ -15,7 +15,7 @@ if "IS_DEV" in os.environ and os.environ['IS_DEV'] == "false": ##? IS_DEV might 
     API_PORT = f"{os.environ['API_PORT']}"
     API_ENDPOINT = f"{os.environ['HTTP']}{os.environ['API_HOST']}:{os.environ['API_PORT']}"
     API_BASEPATH = f"{os.environ['API_BASEPATH']}"
-    SPARQL_ENDPOINT = f"{os.environ['GRAPH_SERVER_HOST']}/repositories/abromics-kg"
+    SPARQL_ENDPOINT = f"{os.environ['GRAPH_SERVER_HOST']}repositories/abromics-kg"
     ADMIN_USERNAME = f"{os.environ['API_ADMIN_USERNAME']}"
     ADMIN_PASSWORD = f"{os.environ['API_ADMIN_PASSWORD']}"
 else:    
